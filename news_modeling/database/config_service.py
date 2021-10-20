0df87b93c9150824671df1dfd8fc2e92cedb7875
seungsu <52397336+seungsu3579@users.noreply.github.com>
2021-10-20 14:13:19 +0900

    Update config_service.py

--- news_modeling/database/config_service.py
+++ news_modeling/database/config_service.py
@@ -1,9 +1,3 @@
 CONFIG = {
-    'RDS_configure': {
-        "postgresql_user": "postgres",
-        "postgresql_password": "kpopstar12!",
-        "postgresql_host": "newsboy-dev.c53g7q6femqw.ap-northeast-2.rds.amazonaws.com",
-        "postgresql_database": "newsboy-platform",
-        "postgresql_port":5432
-    }
+    'RDS_configure': {}
 }
